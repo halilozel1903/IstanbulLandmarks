8ae0bb3c2e2fe947c181de829107b4fd3bfa16a6
halilozel1903@gmail.com <halilozel1903@gmail.com>
2019-08-18 23:12:22 +0300

    logo add, landmarks add

---
+++ IstanbulLandmarks/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  IstanbulLandmarks
+//
+//  Created by Halil Özel on 18.08.2019.
+//  Copyright © 2019 Halil Özel. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellTitle: UILabel!
+    @IBOutlet weak var cellDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
